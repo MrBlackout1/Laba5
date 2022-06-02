c2d4004808a28ae9161c5cb7f400924e827edd42
MrBlackout1 <mrbobfish0@gmail.com>
2022-06-02 14:07:28 +0300

    Initial commit

---
+++ Laba5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+ 
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lst = "Надрукувати всі його слова у порядку спадання їх довжин".Split(' ').ToList();
+            lst = lst.OrderByDescending(x => x.Length).ToList();
+            Console.WriteLine(string.Join(" ", lst));
+            Console.ReadKey();
+        }
+    }
+}
